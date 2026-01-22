686dc077d98c9a3fbc0c34742056707e2cc87141
Rayed <rayed.siddiqui@outlook.com>
2026-01-22 13:32:02 -0800

    Changed Ports

--- 1248C - Code/src/main.cpp
+++ 1248C - Code/src/main.cpp
@@ -8,12 +8,12 @@
 // Chassis constructor
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {1, 2, 3},     // Left Chassis Ports (negative port will reverse it!)
-    {-4, -5, -6},  // Right Chassis Ports (negative port will reverse it!)
+    {8, -9, -10},     // Left Chassis Ports (negative port will reverse it!)
+    {1, 2, -3},  // Right Chassis Ports (negative port will reverse it!)
 
     7,      // IMU Port
-    4.125,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
-    343);   // Wheel RPM = cartridge * (motor gear / wheel gear)
+    3.25,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
+    450);   // Wheel RPM = cartridge * (motor gear / wheel gear)
 
 // Uncomment the trackers you're using here!
 // - `8` and `9` are smart ports (making these negative will reverse the sensor)
@@ -30,8 +30,7 @@ ez::Drive chassis(
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-  // Print our branding over your terminal :D
-  ez::ez_template_print();
+  // Print our branding over your terminal :(
 
   pros::delay(500);  // Stop the user from doing anything while legacy ports configure
 
