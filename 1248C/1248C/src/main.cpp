b0f97e10cb4764d197ebccf2730fc17bd33874c0
LSHenryB <169556674+hbleszynski@users.noreply.github.com>
2026-02-08 10:58:31 -0800

    Modified Op Control for Henry
    reduced sensitivity of the right joystick.

--- 1248C/1248C/src/main.cpp
+++ 1248C/1248C/src/main.cpp
@@ -142,6 +142,7 @@ void opcontrol() {
 	 	// Arcade control scheme
 	 	int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
+		turn = turn * 0.5
 		left_mg.move(dir - turn);                      // Sets left motor voltage
 		right_mg.move(dir + turn);                     // Sets right motor voltage
 
