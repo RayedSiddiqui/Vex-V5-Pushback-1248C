93df5b50cc747a4d0c9ee92a99ef76db3be99076
hbleszynski <henrybleszynski@gmail.com>
2026-02-16 19:01:24 -0800

    skills

--- 1248C - Skills/src/main.cpp
+++ 1248C - Skills/src/main.cpp
@@ -4,6 +4,15 @@
 inline pros::Motor conveyor(20, pros::v5::MotorGear::green);
 inline pros::Motor top_roller(11, pros::v5::MotorGear::green);
 
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::MotorGroup left_mg({-16, 18, 17});    // Creates a motor group with forwards ports 17 & 18 and reversed port 2
+	pros::MotorGroup right_mg({-13, -14, 12});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+
+// Match loader solenoids (ports G and H)
+pros::ADIDigitalOut descorer('G');
+pros::ADIDigitalOut match_loader_solenoid('H');
+
+
 // Conveyor control macros
 #define conveyor_on() conveyor.move(120)
 #define conveyor_off() conveyor.move(0)
@@ -14,9 +23,65 @@ inline pros::Motor top_roller(11, pros::v5::MotorGear::green);
 #define top_roller_off() top_roller.move(0)
 #define top_roller_reverse() top_roller.move(90)
 
-// Match loader solenoids (ports G and H)
-pros::ADIDigitalOut descorer('G');
-pros::ADIDigitalOut match_loader_solenoid('H');
+
+// Turn both on
+#define intake_on() do { conveyor_on(); top_roller_on(); } while(0)
+
+// Store match loads (conveyor on, top roller in reverse at half speed)
+#define store_match_load() do { conveyor.move(120); top_roller.move(55); } while(0)
+
+// autonomous movement helper + macros
+#define drive_ms(l, r, ms) do { left_mg.move(l); right_mg.move(r); pros::delay(ms); left_mg.move(0); right_mg.move(0); } while(0)
+#define turnright(speed, ms) drive_ms((speed), -(speed), (ms))
+#define turnleft(speed, ms) drive_ms(-(speed), (speed), (ms))
+#define forward(speed, ms) drive_ms((speed), (speed), (ms))
+#define backward(speed, ms) drive_ms(-(speed), -(speed), (ms))
+#define stop() do { left_mg.move(0); right_mg.move(0); pros::delay(50); } while(0)
+#define score() do { top_roller_on(); pros::delay(300); top_roller_off(); } while(0)
+#define jiggle() do { left_mg.move(-50); right_mg.move(-50); pros::delay(100); left_mg.move(50); right_mg.move(50); pros::delay(100); } while(0)
+#define lower_match_loader() do { match_loader_solenoid.set_value(true); pros::delay(100); } while(0)
+#define raise_match_loader() do { match_loader_solenoid.set_value(false); pros::delay(100); } while(0)
+
+#define load_score() do { \
+		top_roller_reverse(); \
+		conveyor_on(); \
+		backward(90, 200); \
+		stop();	\
+		lower_match_loader(); \
+		backward(90, 100);\
+		stop();\
+		pros::delay(500); \
+		jiggle(); \
+		jiggle(); \
+		forward(90, 100); \
+		stop(); \
+		raise_match_loader(); \
+		forward(90, 200); \
+		stop(); \
+		score(); \
+		backward(90, 50); \
+		stop(); } while(0)
+
+#define traverse_long_goal() do {turnright(90, 90); \
+		forward(100, 50); \
+		stop(); \
+		turnleft(100, 90); \
+		stop(); \
+		forward(100, 500); \
+		stop(); \
+		turnleft(100, 90); \
+		stop(); \
+		forward(100, 50); \
+		stop();\
+		turnleft(100, 90);} while(0)
+	
+#define traverse_match_load() do {turnright(90, 90); \
+		stop(); \
+		forward(90, 500); \
+		stop();\
+		turnleft(90, 90);} while(0)
+
+
 
 // Turn both on
 #define intake_on() do { conveyor_on(); top_roller_on(); } while(0)
@@ -25,22 +90,37 @@ pros::ADIDigitalOut match_loader_solenoid('H');
 #define store_match_load() do { conveyor.move(120); top_roller.move(55); } while(0)
 
 // A simple autonomous function that drives forward for a short time
-void dummy_auto() {
-	pros::MotorGroup left_mg({19, 18, -17});
-	pros::MotorGroup right_mg({-13, 14, -12});
+void skeleton_auto() {
+	//Still need figure outfirst move here.
 
-	// Drive for 100ms to approximate 2 inches
-	top_roller_reverse();
-	conveyor_on();
-	left_mg.move(-100);
-	right_mg.move(-100);
-	pros::delay(300);  
-	left_mg.move(0);
-	right_mg.move(0);
-	top_roller_off();
-	conveyor_off();
+
+
+	load_score(); //First Load
+
+	traverse_long_goal();
+	 
+	load_score(); //Second Load and Score
+
+
+	traverse_match_load();
+	
+	load_score();  //Third Load and Score
+	
+	traverse_long_goal();
+	
+	load_score();  //Fourth Load and Score
+
+	turnright(90, 90);
+	stop();
+	forward(90, 100);
+	stop();
+	turnright(90, 90);
+	stop();
+	forward(90, 200);
+
 	
 }
+
 
 /**
  * A callback function for LLEMU's center button.
@@ -101,7 +181,7 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-	dummy_auto();
+	skeleton_auto();
 }
 
 /**
@@ -117,12 +197,10 @@ void autonomous() {
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol() {
-	
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({-16, 18, 17});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-13, -14, 12});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
+/*void opcontrol() {
+	
+	
 	// State variables for toggles
 	bool conveyor_enabled = false;
 	bool roller_enabled = false;
@@ -216,3 +294,5 @@ void opcontrol() {
 	 	pros::delay(20);  // Run for 20 ms then update
 	 }
 }
+
+*/
